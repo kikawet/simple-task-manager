ca7c146e4442487150b61dda81fd5d93fbec21f8
kikawet <lopezortegafrancisco1@gmail.com>
2023-11-10 22:06:08 +0100

    implement basic 404

--- src/views/ErrorNotFound.vue
+++ src/views/ErrorNotFound.vue
@@ -1,1 +1,12 @@
-<template>404</template>
+<script setup lang="ts">
+import { routes } from '@/router'
+</script>
+
+<template>
+  <main class="h-full bg-background flex flex-col justify-center items-center gap-4">
+    <h1 class="text-textColor-400 text-9xl">404</h1>
+    <h1 class="text-textColor-400 text-4xl">
+      Page not found go to <RouterLink class="underline" :to="routes.List">home</RouterLink>
+    </h1>
+  </main>
+</template>
